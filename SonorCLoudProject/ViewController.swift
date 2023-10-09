d8089998f222c3702615b824ee5ab487350c1458
MandaBhaskaraKiran <Manda.unvvbhaskarakiran@realpage.com>
2023-10-09 15:37:57 +0530

    first code base

---
+++ SonorCLoudProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SonorCLoudProject
+//
+//  Created by Manda U N V V Bhaskara Kiran on 09/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
